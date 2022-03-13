6b2b885c06130710105137fc5d2873ce9f9e6e58
sdelon <delon.steph@gmail.com>
2022-03-13 12:20:10 +0100

    3 files modified

--- main.js
+++ main.js
@@ -1,72 +1,66 @@
 // Variables
-const container = document.querySelector('.container');
-const newTaskInput = document.getElementById('new_task_input')
-const taskform = document.getElementById('new_task_form');
-const tasksList = document.getElementById('tasksList');
-const taskBtns = document.querySelectorAll('.task_check_btn');
+const container = document.querySelector('.todo-vide');
+const nouveau_todo = document.querySelector('#nouveau_todo');
+const form = document.querySelector('#form_todo');
+const todo_liste = document.querySelector('#liste_todo');
 
 // Do this when we submit the form
-taskform.addEventListener('submit', function (e) {
+form.addEventListener('submit', function (e) {
 
     e.preventDefault();
-    const newtaskInputValue = taskform.elements.new_task_input;
-
-    addTask(newtaskInputValue.value);
+    const nouveau_todo_valeur = form.elements.nouveau_todo;
+    ajout_todo(nouveau_todo_valeur.value);
 
     // Reset input value to empty
-    newtaskInputValue.value = '';
-    container.classList.remove('task_list_empty');
-
+    nouveau_todo_valeur.value = '';
 })
 
 // To  add task in List
-function addTask(newTask) {
+function ajout_todo(todo_contenu) {
 
+    container.classList.add('invisible');
     // Create li element and set its class
-    const newTaskItem = document.createElement('li');
-    newTaskItem.setAttribute('class', 'task_item');
+    const nouveau_todo_conteneur = document.createElement('li');
+    nouveau_todo_conteneur.setAttribute('class', 'todo');
 
     // Create checkbox  element and set its type and  class 
 
-    const newCheckBtn = document.createElement('div');
-    newCheckBtn.setAttribute('class', 'task_check_btn')
+    const nouveau_todo_btn_complet = document.createElement('div');
+    nouveau_todo_btn_complet.setAttribute('class', 'todo-fini-btn')
 
     // Create span  element and set its class and add new task input
-    const newTaskBio = document.createElement('span');
-    newTaskBio.setAttribute('class', 'task_bio')
+    const nouveau_todo_texte = document.createElement('span');
+    nouveau_todo_texte.setAttribute('class', 'todo-texte')
     // Put value of input in it
-    newTaskBio.innerText = newTask; // putting value of input in the li
+    nouveau_todo_texte.innerText = todo_contenu; // putting value of input in the li
 
     // append (insert) li tag in Ul
-    tasksList.appendChild(newTaskItem)
+    todo_liste.appendChild(nouveau_todo_conteneur);
     // append (insert) checkbox in li
-    newTaskItem.append(newCheckBtn , newTaskBio)
+    nouveau_todo_conteneur.append(nouveau_todo_btn_complet, nouveau_todo_texte);
 
     // Run this function when task is completed or checkbox is checked
-    onTaskComplete(newCheckBtn)
+    supprimer_todo(nouveau_todo_btn_complet)
 
 }
 
 // To remove the completed task
-function onTaskComplete(btns) {
+function supprimer_todo(todo_btns) {
 
-    btns.addEventListener('click', function (e) {
+    todo_btns.addEventListener('click', function (e) {
 
         const parent = e.target.parentElement;
-        parent.classList.add('task-completed'); // To slide out the task to the right
+        parent.classList.add('todo-fini'); // To slide out the task to the right
         // Now we delete that tast which we have slided out
         setTimeout(() => {
             // Removing Parent Element of checkobx which is Li in 0.5 s
             parent.remove();
         }, 400);
 
-
-        if (tasksList.childNodes.length == 1) {
+        if (todo_liste.childNodes.length == 1) {
             setTimeout(() => {
-                container.classList.add('task_list_empty')
-
+                container.classList.remove('invisible');
             }, 800);
         }
-
     })
 }
